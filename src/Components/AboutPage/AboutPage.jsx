0577695d5017c90a34c3e848b1397ea787edf30b
Ammar Kashif <ammar.kashif@torontomu.ca>
2024-08-04 00:08:40 -0400

    Updates Styles and Designs

--- src/Components/AboutPage/AboutPage.jsx
+++ src/Components/AboutPage/AboutPage.jsx
@@ -35,13 +35,13 @@ function AboutPage() {
 
   return (
     <>
-      <div className="about-page">
-        <h1 className="page-title">About Page</h1>
+      <div className="about-page-element">
+        <h1 className="page-title">About Us</h1>
 
         <p className="site-description">
-        Welcome to <strong>GreenGuide</strong>, your dedicated partner in revolutionizing waste management and recycling 
+        Welcome to <strong>PlanetPal</strong>, your dedicated partner in revolutionizing waste management and recycling 
         practices. Our website is designed to simplify the complexities of recycling and promote sustainable 
-        waste management. At WasteWise, we are passionate about making recycling accessible and straightforward 
+        waste management. At <strong>PlanetPal</strong>, we are passionate about making recycling accessible and straightforward 
         for everyone. Our innovative platform provides practical tools, step-by-step guides, and educational 
         resources to help you sort, recycle, and reduce waste efficiently. We believe that small changes in 
         how we manage our waste can lead to significant environmental benefits. Our goal is to support individuals 
@@ -50,57 +50,41 @@ function AboutPage() {
         </p>
 
         <h2 className="creators-title">Creators</h2>
+        
+        <div className="floating-object"></div> {/* Add this line for the floating object */}
+
 
         <div className="profile">
           <img src={andyPic} className="profile-pic"/>
           <div className="profile-info">
-            <name>Andy</name>
-            <p>Andy Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
-              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
-              anim id est laborum..</p>
+            <span className="name">Andy Pak</span>
+            <p>I am a programmer who is good at using Ruby, React and JavaScript. I love developing front-end, creating interfaces that are smooth to interact with for users. What makes me happy is being thorough when coding so that all projects I get involved in become perfect.</p>
           </div>
         </div>
 
         <div className="profile reverse">
           <img src={joshPic} alt="Josh" className="profile-pic"/>
           <div className="profile-info">
-            <name>Josh</name>
-            <p>Josh Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque 
-              laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto 
-              beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut 
-              odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
-              Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
-              sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat 
-              voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.</p>
+            <span className="name">Josh Xue</span>
+            <p>I know Python well and also have a background in HTML, CSS as well as JavaScript. My interest lies in combining front end development with back end functionality which gives complete solutions which are appealing to look at and technically sound too. I like my job where these technologies merge together.
+            </p>
           </div>
         </div>
 
         <div className="profile">
           <img src={ammarPic} alt="Ammar" className="profile-pic"/>
           <div className="profile-info">
-            <name>Ammar</name>
-            <p>Ammar At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium 
-              voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati 
-              cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est 
-              laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero 
-              tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime 
-              placeat facere possimus.</p>
+            <span className="name">Ammar Kashif</span>
+            <p>I am a committed programmer who is always ready to tackle issues related to Java, Python, JavaScript , HTML and CSS. I am working on my skills in Python with the goal of striking a balance between that language and software development. My intent for self-development and learning backed by technical competence pushes me to give my best in every project that I do.</p>
           </div>
         </div>
 
         <div className="profile reverse">
           <img src={yuktiPic} alt="Yukti" className="profile-pic"/>
           <div className="profile-info">
-            <name>Yukti</name>
-            <p>Yukti Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam libero tempore, cum soluta 
-              nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, 
-              omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis 
-              debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non 
-              recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores 
-              alias consequatur aut perferendis doloribus asperiores repellat.</p>
+            <div className="name">Yukti</div>
+            <p>
+            I am a seasoned developer proficient in both backend and frontend languages. Coding thrills me; it gives me an adrenaline rush when solving difficult problems leading to my nickname “an extreme coder”. All through the project, my focus is on delivering high quality solutions by applying my broad range of skills.</p>
           </div>
         </div>
       </div>
